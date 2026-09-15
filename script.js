6f2224ed0d9460b3cb55e39b13860bc844bd9de3
Mol4868 <amirplay09@gmail.com>
2026-09-15 10:00:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const cat =
-  document.querySelector("#cat");
-const catchangebutton =
-  document.querySelector("#CatChanger");
-catchangebutton.addEventListener("click", () => {
-  cat.src = "images/Maine_Coon_Cat.jpg";
+const cat = 
+  document.querySelector('#cat'); 
+const catchangebutton = 
+  document.querySelector('#CatChanger'); 
+catchangebutton.addEventListener('click', () => { 
+  cat.src = 'images/Maine_Coon_Cat.jpg'; 
 });
